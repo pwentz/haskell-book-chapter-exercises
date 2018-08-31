35b35fed34e82fbebc1cf4662e2849c50a8f7ed3
pwentz <patrick.w.wentz@gmail.com>
2018-08-30 22:08:57 -0500

    - chapter exercises for state monad

--- state.hs
+++ state.hs
@@ -1,12 +1,44 @@
 {-# LANGUAGE InstanceSigs #-}
 
-newtype Moi s a = Moi
-    { runMoi :: s -> (a, s)
-    }
+newtype State s a =
+  State { runState :: s -> (a, s) }
 
-instance Functor (Moi s) where
-    fmap :: (a -> b) -> Moi s a -> Moi s b
-    fmap f (Moi g) =
-        Moi $ \s ->
-            let (state, init) = g s
-            in (f state, init)
+instance Functor (State s) where
+  fmap :: (a -> b) -> State s a -> State s b
+  fmap f (State g) = State $ \s -> let (val, newState) = g s in (f val, newState)
+
+instance Applicative (State s) where
+  pure :: a -> State s a
+  pure a = State ((,) a)
+
+  (<*>) :: State s (a -> b) -> State s a -> State s b
+  (State f) <*> (State g) =
+    State $ \s ->
+      let (val, newState) = g s
+          (newFn, _) = f s
+      in (newFn val, newState)
+
+instance Monad (State s) where
+  (>>=) :: State s a -> (a -> State s b) -> State s b
+  (State g) >>= f =
+    State $ \s ->
+      let (val, _) = g s
+          (State getFinal) = f val
+      in getFinal s
+
+get :: State s s
+get = State $ \s -> (s, s)
+
+put :: s -> State s ()
+put x = State $ \_ -> ((), x)
+
+exec :: State s a -> s -> s
+exec (State f) s = snd (f s)
+
+eval :: State s a -> s -> a
+eval (State f) s = fst (f s)
+
+modify :: (s -> s) -> State s ()
+modify f =
+  State $ \s -> ((), f s)
+  -- get >>= (put . f)
